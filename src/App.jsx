c8a7cb942883f0ddedb4614165d2b7922731f3a8
Camilo Torres <racknarow1@gmail.com>
2022-07-04 17:01:39 -0500

    add Routerx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Layout from './layout/Layout'
+import Inicio from './paginas/Inicio'
+import NuevoCliente from './paginas/NuevoCliente'
+import EditarCliente from './paginas/EditarCliente'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <h1 className='text-6xl font-extrabold'>CRM REACT</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route  path='/clientes' element={<Layout />} >   
+            <Route index element={<Inicio />} />  {/* RUTAS ANIDADAS */}
+            <Route path='nuevo' element={<NuevoCliente />} />  {/* RUTAS ANIDADAS */}
+            <Route path='editar/:id' element={<EditarCliente />} />  {/* RUTAS ANIDADAS */}
+        </Route>
+            
+      </Routes>
+    </BrowserRouter>
   )
 }
 
