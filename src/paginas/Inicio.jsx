bf9509d5d74f3d5e06fce6673ed34d2974d959b6
Camilo Torres <racknarow1@gmail.com>
2022-07-04 18:24:42 -0500

    detch clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 
 const Inicio = () => {
+
+    const [clientes, setClientes] = useState([])
+
+    useEffect(() => {
+
+        const obtenerClientesAPI = async () => {
+
+            try {
+                
+                const url = 'http://localhost:4000/clientes'
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+
+                setClientes(resultado);
+
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        obtenerClientesAPI();
+    } , [])
+
   return (
     <div>
       <h1>Inicio</h1>
