91279a422352a62dd0f996ae5bd49e7cdf9386b2
Camilo Torres <racknarow1@gmail.com>
2022-07-04 13:26:10 -0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.js", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
